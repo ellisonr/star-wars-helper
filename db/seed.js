52e56bffe529f699cf3f666e561dd94ad9a4f09d
Ellison Rio <rio.ellison@gmail.com>
2019-06-05 19:14:10 -0400

    seed.js foundation

---
+++ db/seed.js
@@ -0,0 +1,10 @@
+const mongoose = require("./connection");
+
+const charactersData = require("./characters.json");
+const filmsData = require("./films.json");
+const vehiclesData = require("./vehicles.json");
+
+const Character = require("./models/Character");
+const Film = require("./models/Film");
+const Vehicle = require("./models/Vehicle");
+
