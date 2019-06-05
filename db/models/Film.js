0b41a1b3e25a3fe95f68fd39959f99119f1394a7
Ellison Rio <rio.ellison@gmail.com>
2019-06-05 19:21:08 -0400

    more models foundation

--- db/models/Film.js
+++ db/models/Film.js
@@ -1,3 +1,9 @@
 const mongoose = require("../connection");
 
+const FilmSchema = new mongoose.Schema({
+    
+})
+
+const Film = mongoose.model("Film", FilmSchema);
+
 module.exports = Film;
--- db/models/Vehicle.js
+++ db/models/Vehicle.js
@@ -1,3 +1,9 @@
 const mongoose = require("../connection");
 
+const VehicleSchema = new mongoose.Schema({
+    
+})
+
+const Vehicle = mongoose.model("Vehicle", VehicleSchema);
+
 module.exports = Vehicle;
