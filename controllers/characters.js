b067b2821acf726077de6c8f7cd3108607ce1240
Ellison Rio <rio.ellison@gmail.com>
2019-06-05 22:04:58 -0400

    added routes to character controller

--- controllers/characters.js
+++ controllers/characters.js
@@ -3,4 +3,31 @@ const router = express.Router();
 
 const Character = require("../db/models/Character");
 
+router.get("/", (req, res) => {
+    Character.find({}).then(characters => res.json(characteres));
+});
+
+router.get("/:name", (req, res) => {
+    Character.find({ name: req.params.name})
+    .then(characters => res.json(bookmarks));
+});
+
+router.post("/", (req,res) => {
+    Character.create(req.body)
+    .then(character => res.json(character));
+});
+
+router.put("/:name", (req, res) => {
+    Character.findOneAndUpdate({ name: req.params.name}, req.body,
+    {new: true})
+    .then(character => res.json(character));
+});
+
+router.delete("/:name", (req, res) => {
+    Character.findOneAndDelete({ name: req.params.name})
+    .then(character => {
+    res.json(character);
+    });
+});
+
 module.exports = router;
